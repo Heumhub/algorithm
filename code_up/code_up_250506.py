747981dcc9dd28f342c6241b3e356962a8efa323
heum <jdh6983@naver.com>
2025-05-06 20:49:39 +0900

    Create code_up_250506.py

---
+++ code_up/code_up_250506.py
@@ -0,0 +1,31 @@
+# 6065 : [기초-조건/선택실행구조] 정수 3개 입력받아 짝수만 출력하기(설명)(py)
+
+a, b, c = input().split()
+
+a = int(a)
+b = int(b)
+c = int(c)
+
+if a % 2 == 0:
+    print(a)
+
+if b % 2 == 0:
+    print(b)
+
+if c % 2 == 0:
+    print(c)
+
+
+a, b, c = map(int, input().split())
+
+if a % 2 == 0:
+    print(a)
+
+if b % 2 == 0:
+    print(b)
+
+if c % 2 == 0:
+    print(c)
+# 들여쓰기를 꼭 해야한다.
+
+
