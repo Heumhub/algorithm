1f979e380f9c555de9fd0cb0b5c9903a090dd915
heum <jdh6983@naver.com>
2025-05-09 22:44:11 +0900

    Create code_up_250509.py

---
+++ code_up/code_up_250509.py
@@ -0,0 +1,14 @@
+# 6069 : [기초-조건/선택실행구조] 평가 입력받아 다르게 출력하기(py)
+
+grade = input()
+
+if grade == 'A':
+    print("best!!!")
+elif grade == 'B':
+    print("good!!")
+elif grade == 'C':
+    print("run!")
+elif grade == 'D':
+    print("slowly~")
+else:
+    print("what?")
